3c2fa0b1040dcda1400c6832b82817c91f4438d9
Derek Z <Derek Z@DESKTOP-8JHHR0R>
2017-05-01 16:12:39 -0400

    Commit 5

--- OZHI-AWDS/Classes/EstimateDataSet.cs
+++ OZHI-AWDS/Classes/EstimateDataSet.cs
@@ -9,32 +9,41 @@ namespace OZHI_AWDS.Classes
     public class EstimateDataSet
     {
         public string Service { get; set; }
-        public string ClientID { get; set; }
+        public string ServiceDescription { get; set; }
         public string Project { get; set; }
-        public string Address { get; set; }
-        public string LegalDescription { get; set; }
-        public string City { get; set; }
-        public string Province { get; set; }
-        public string PostalCode { get; set; }
+        public string ClientID { get; set; }
+        public string AlternateName { get; set; }
         public string LoanType { get; set; }
         public string LoanTypeF { get; set; }
+        public string Contact { get; set; }
         public string Telephone { get; set; }
+        public string Address { get; set; }
         public string CMHCAccountNumber { get; set; }
+        public string LegalDescription { get; set; }
         public string AgentReferenceNumber { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
         public string InspectionDate { get; set; }
+        public string PostalCode { get; set; }
         public string Inspector1 { get; set; }
         public string Inspector2 { get; set; }
-        public class TablixClass
-        {
-            public string WorkSpecification { get; set; }
-            public string Priority { get; set; }
-            public string ImperialDescription { get; set; }
-            public string MetricDescription { get; set; }
-            public string ImperialQuanity { get; set; }
-            public string MetricQuanity { get; set; }
-            public string MaterialEstimate { get; set; }
-            public string LabourEstimate { get; set; }
-            public string WorkSpecificationDescription { get; set; }
-        }
+        public string ItemCode { get; set; }
+        public string WorkSpecification { get; set; }
+        public string Estimate { get; set; }
+        public string WorkNumber { get; set; }
+        public string Priority { get; set; }
+        public string Section { get; set; }
+        public string Urgency { get; set; }
+        public string SpecialText { get; set; }
+        public string ImperialUnits { get; set; }
+        public string MetricDescription { get; set; }
+        public string ImperialDescription { get; set; }
+        public string MetricDescription { get; set; }
+        public string ImperialQuanity { get; set; }
+        public string MetricQuanity { get; set; }
+        public string MaterialEstimate { get; set; }
+        public string LabourEstimate { get; set; }
+        public string WorkSpecificationDescription { get; set; }
+        
     }
 }
--- OZHI-AWDS/Pages/ReportViewerEstimate.xaml.cs
+++ OZHI-AWDS/Pages/ReportViewerEstimate.xaml.cs
@@ -47,7 +47,15 @@ namespace OZHI_AWDS.Pages
             dt.Columns.Add(new DataColumn("InspectionDate", typeof(string)));
             dt.Columns.Add(new DataColumn("Inspector1", typeof(string)));
             dt.Columns.Add(new DataColumn("Inspector2", typeof(string)));
-            dt.Columns.Add(new DataColumn("Tablix", typeof(OZHI_AWDS.Classes.EstimateDataSet.TablixClass)));
+            dt.Columns.Add(new DataColumn("WorkSpecification", typeof(string)));
+            dt.Columns.Add(new DataColumn("Priority", typeof(string)));
+            dt.Columns.Add(new DataColumn("ImperialDescription", typeof(string)));
+            dt.Columns.Add(new DataColumn("MetricDescription", typeof(string)));
+            dt.Columns.Add(new DataColumn("ImperialQuanity", typeof(string)));
+            dt.Columns.Add(new DataColumn("MetricQuanity", typeof(string)));
+            dt.Columns.Add(new DataColumn("MaterialEstimate", typeof(string)));
+            dt.Columns.Add(new DataColumn("LabourEstimate", typeof(string)));
+            dt.Columns.Add(new DataColumn("WorkSpecificationDescription", typeof(string)));
         }
     }
 }
